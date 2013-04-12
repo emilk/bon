67d13177883d41b3a235d4ea6708570ee672b714
emilk <emil.ernerfeldt@gmail.com>
2013-04-12 19:19:07 +0200

    Updated to new API function names

--- examples/hello.c
+++ examples/hello.c
@@ -3,14 +3,14 @@
 
 int main() {
 	FILE* fp = fopen("hello.bon", "wb");
-	bon_w_doc* B = bon_w_new_doc(&bon_file_writer, fp, BON_W_FLAG_DEFAULT );
+	bon_w_doc* B = bon_w_new(&bon_file_writer, fp, BON_W_FLAG_DEFAULT );
 	
-	bon_w_begin_obj(B);  // The root object
+	bon_w_obj_begin(B);  // The root object
 	bon_w_key(B, "msg");
 	bon_w_cstring(B, "Hello world!");
-	bon_w_end_obj(B);
+	bon_w_obj_end(B);
 	
-	bon_error err = bon_w_close_doc( B );
+	bon_error err = bon_w_close( B );
 	if (err != BON_SUCCESS) {
 		fprintf(stderr, "Failed to write to hello.bon: %s", bon_err_str(err));
 	}
